df75d554f0c17945c9a2725f57b2d93195f160bd
Aayush Chhetri <aayush.chhetri.81@gmail.com>
2024-06-24 08:59:10 +0545

    update accordion

--- accordion/src/components/list.jsx
+++ accordion/src/components/list.jsx
@@ -1,35 +1,3 @@
-// import { useState } from "react";
-// import datas from "../data/data";
-// import styles from "./list.module.css";
-
-// function ListComponent() {
-//   const [selected, setSelected] = useState(null);
-
-//   function handleOnClick(id) {
-//     setSelected(selected === id ? null : id);
-//   }
-
-//   return (
-//     <div>
-//       {datas.map((dataItem, dataIndex) => (
-//         <div key={dataIndex}>
-//           <div
-//             key={dataIndex}
-//             className={`${styles.customDiv}`}
-//             onClick={() => handleOnClick(dataItem.id)}
-//           >
-//             {" "}
-//             {dataItem.question} <span className={`${styles.add}`}>+</span>
-//           </div>
-//           {selected === dataItem.id ? <div>{dataItem.answer} </div> : null}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default ListComponent;
-
 import { useState } from "react";
 import datas from "../data/data";
 import styles from "./list.module.css";
@@ -38,7 +6,7 @@ function ListComponent() {
   const [selected, setSelected] = useState(null);
 
   function handleOnClick(id) {
-    setSelected(selected === id ? null : id); // Toggle selected state
+    setSelected(selected === id ? null : id);
   }
 
   return (
